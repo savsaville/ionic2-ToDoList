34053f657adf0df5e39a2de0f154709f38f6c842
Richard Saville <richardasaville@gmail.com>
2017-07-11 11:21:25 +1000

    added classes on home page and edit.todo

--- src/pages/edit-todo/edit-todo.ts
+++ src/pages/edit-todo/edit-todo.ts
@@ -1,12 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the EditTodoPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
 @IonicPage()
 @Component({
   selector: 'page-edit-todo',
@@ -14,11 +8,23 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class EditTodoPage {
 
+  todo: any;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+
+    this.todo = {
+      title: '',
+      description: ''
+    };
+
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad EditTodoPage');
+
+  }
+
+  save(){
+    console.log('TODO: finish save function');
   }
 
 }
